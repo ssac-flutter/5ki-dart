eada20fde0f0a9e0d0f92419b4158b9863fcefcc
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-05-30 15:22:00 +0900

    sort

---
+++ lib/sort.dart
@@ -0,0 +1,24 @@
+import 'package:dart_basic/hero.dart';
+
+void main() {
+  final heroes = [
+    Hero(name: '홍길동'),
+    Hero(name: '이순신'),
+    Hero(name: '김삿갓'),
+  ];
+
+  // 방법 1.
+  heroes.sort((a, b) => a.name.compareTo(b.name));
+
+  // 방법 2.
+  heroes.sort((a, b) => Comparable.compare(a.name, b.name));
+
+  // 방법 3.
+  heroes.sort(compareHero);
+
+  print(heroes);
+}
+
+int compareHero(Hero hero1, Hero hero2) {
+  return hero1.name.compareTo(hero2.name);
+}
