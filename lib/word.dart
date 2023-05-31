05b9e1b9cea293d644a82dcf470a22bb9f1501b4
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-05-31 15:58:52 +0900

    모음인지 정답

--- lib/word.dart
+++ lib/word.dart
@@ -1,18 +1,13 @@
 class Word {
   String word = '';
 
-  bool isVowel(int i) {
-    return word.substring(i, i + 1) == 'a';
-  }
+  bool isVowel(int i) =>
+      word.substring(i, i + 1).contains(RegExp(r'[aeiouAEIOU]'));
 }
 
 void main() {
   Word word = Word();
   word.word = 'apple';
-  
-  print(word.isVowel(0)); // true
-  print(word.isVowel(1));
-  print(word.isVowel(2));
-  print(word.isVowel(3));
-  print(word.isVowel(4)); // true
+
+  print(word.isVowel(0));
 }
