25ffbbf631dd01d38117a64f70f984d382e53898
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-13 14:31:48 +0900

    PostMemoryRepositoryImpl 테스트 샘플 2

--- test/unit_test/exam5/repository/post_memory_repository_test.dart
+++ test/unit_test/exam5/repository/post_memory_repository_test.dart
@@ -4,6 +4,50 @@ import 'package:dart_basic/unit_test/exam5/repository/post_repository.dart';
 import 'package:test/test.dart';
 
 void main() {
+  PostRepository _repository = PostMemoryRepositoryImpl();
+
+  setUp(() async {
+    _repository.addPost(Post(0, 'test1', 'test1'));
+    _repository.addPost(Post(1, 'test2', 'test2'));
+  });
+
+  tearDown(() async {
+    _repository = PostMemoryRepositoryImpl();
+  });
+
+
+  group('PostRepository', () {
+    test('getPosts()', () async {
+      final posts = await _repository.getPosts();
+      expect(posts.length, 2);
+    });
+
+    test('addPost()', () async {
+      await _repository.addPost(Post(2, 'test3', 'test3'));
+
+      var posts = await _repository.getPosts();
+      expect(posts.length, 3);
+
+      await _repository.addPost(Post(3, 'test4', 'test4'));
+
+      posts = await _repository.getPosts();
+      expect(posts.length, 4);
+    });
+
+    test('updatePost()', () async {
+      await _repository.updatePost(Post(0, '0', '0'));
+      final firstPost = (await _repository.getPosts())[0];
+      expect(firstPost.title, '0');
+    });
+
+    test('deletePost()', () async {
+      final post = Post(0, 'test1', 'test1');
+      await _repository.deletePost(post);
+      final posts = await _repository.getPosts();
+      expect(posts.length, 1);
+    });
+  });
+
   test('PostMemoryRepository 테스트', () async {
     // 준비
     final PostRepository repository = PostMemoryRepositoryImpl();
@@ -20,8 +64,6 @@ void main() {
     expect(firstPost.title, 'test2');
 
     await repository.deletePost(post);
-    expect(posts.length, 0);
-
-
+    expect(posts.length, 1);
   });
 }
