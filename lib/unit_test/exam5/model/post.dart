cdee2591bfdb83213b1425e4787f9234f2d6f13b
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-13 14:31:48 +0900

    PostMemoryRepositoryImpl 테스트 샘플 2

--- lib/unit_test/exam5/model/post.dart
+++ lib/unit_test/exam5/model/post.dart
@@ -4,4 +4,12 @@ class Post {
   String body;
 
   Post(this.id, this.title, this.body);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Post && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
